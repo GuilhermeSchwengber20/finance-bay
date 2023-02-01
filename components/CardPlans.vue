6b529ac1721419ea32705dfd06c8812a64681270
Guilherme Schwengber <guilhermenft2021@gmail.com>
2023-02-01 11:55:07 -0300

    Adicionando html e estilização dos planos

---
+++ components/CardPlans.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="containerCard">
+    <div class="cardHeader">
+        <div>
+            <span id="category"># Category</span>
+        </div>
+        <div class="planTitleContainer">
+            <span id="planTitle">Trip to Waduk Cengkik</span>
+        </div>
+    </div>
+    <div class="cardBody">
+        <div class="priceContainer">
+            <div>
+                <span id="obtainedVale">$249</span>
+            </div>
+            <div>
+                <span id="finalValue">$400</span>
+            </div>
+        </div>
+        <div class="bgProgress">
+            <div class="progressbar">
+                <div class="point"></div>
+            </div>
+        </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CardPlans",
+}
+</script>
+<style scoped>
+
+.containerCard{
+    min-width: 250px;
+    padding: 10px;
+    height: auto;
+    background-color: #FFF;
+    border-radius: 15px;
+    box-shadow: 1px 1px 1px 1px black;
+}
+
+#category{
+    color:blueviolet;
+}
+
+.planTitleContainer{
+    margin-top: 3px;
+}
+#planTitle{
+    font-size: 16px;
+    font-weight: 700;
+}
+.priceContainer{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 10px 5px;
+    margin-top: 18px;
+}
+
+#obtainedVale{
+    font-weight: 700;
+    font-size: 16px;
+}
+#finalValue{
+    font-weight: 700;
+    font-size: 16px;
+    color: var(--arrow);
+}
+
+.bgProgress{
+    background-color: #e6e6e6;
+    width: 220px;
+    height: 15px;
+    margin: auto;
+    border-radius: 15px;
+}
+
+.progressbar{
+    width: 80%;
+    background-color: blueviolet;
+    height: 15px;
+    border-radius: 15px;
+    display: flex;
+    flex-direction: row;
+
+
+    justify-content: flex-end;
+    align-items: center;
+}
+
+.point{
+    width: 12px;
+    height: 12px;
+    background-color: #FFF;
+    border-radius: 50%;
+    margin-right: 3px;
+}
+</style>
---
+++ components/MyPlan.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="containerPlan">
+    <div class="headerPlan">
+      <div>
+        <span>My plans</span>
+      </div>
+      <div>
+        <span>View all</span>
+      </div>
+    </div>
+    <div class="bodyPlan" id="containerPlan">
+      <CardPlans />
+      <CardPlans />
+      <CardPlans />
+      <CardPlans />
+      <CardPlans />
+      <CardPlans />
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: "MyPlan",
+  mounted(){
+    this.carrouselEvent();
+  },
+  methods: {
+    carrouselEvent(){
+      const div = document.getElementById("containerPlan");
+      div.addEventListener("touchstart", (e) => {
+        console.log(e);
+      })
+    }
+  },
+}
+</script>
+<style scoped>
+.containerPlan{
+  width: 100%;
+  height: 25vh;
+  overflow-x: hidden;
+}
+.headerPlan{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 0px 25px;
+}
+.headerPlan div:nth-child(1) span{
+  font-size: 1.1rem;
+  font-weight: 600;
+}
+
+.bodyPlan{
+  margin-top: 10px;
+  width: 100%;
+  overflow-x: auto;
+  padding: 5px 25px;
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  flex-wrap: nowrap;
+}
+</style>
